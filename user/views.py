0456148acc36b96eb3f1b21415ef5cc9795ceb16
Roman Sokolov <gnonasis@gmail.com>
2026-05-25 08:36:38 +0300

    add GoogleLogin view for google auth

--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 
-# Create your views here.
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://127.0.0.1:8000/" # URL твого фронтенду, куди редіректить Google
+    client_class = OAuth2Client
