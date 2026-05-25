d546f4099e6621cab183e54bb0dc96cc0e251c5f
Roman Sokolov <gnonasis@gmail.com>
2026-05-25 08:31:17 +0300

    change user model and create CustomUserManager to implement email instead username

--- user/models.py
+++ user/models.py
@@ -1,5 +1,85 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.apps import apps
+from django.contrib import auth
+from django.contrib.auth.hashers import make_password
+
+
+
+class CustomUserManager(UserManager):
+    use_in_migrations = True
+
+    def _create_user_object(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        if username:
+            username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        return user
+
+    def create_user(self, email, username=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    create_user.alters_data = True
+
+    async def acreate_user(self, email, username=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return await self._acreate_user(username, email, password, **extra_fields)
+
+    acreate_user.alters_data = True
+
+    def create_superuser(self, email, username=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+    create_superuser.alters_data = True
+
+    async def acreate_superuser(
+        self, email, username=None, password=None, **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return await self._acreate_user(username, email, password, **extra_fields)
+
+    acreate_superuser.alters_data = True
+
+
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(_("email address"), unique=True)
+
+    # Робимо username необов'язковим
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # Для superuser-а через термінал тепер потрібні лише email та password
+    objects = CustomUserManager()
+
