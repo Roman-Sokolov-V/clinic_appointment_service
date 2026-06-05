e1b0f16d834c91ef2a8a9a7d4dcebb40eadfc7ab
Roman Sokolov <gnonasis@gmail.com>
2026-06-05 08:08:13 +0300

    add success, cancel pathes

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from payment.views import webhook_view
+from payment.views import webhook_view, success_view, cancel_view
 
 app_name = 'payment'
 
 urlpatterns = [
     path("webhook/", webhook_view, name="payment_webhook"),
+    path("success/", success_view, name="success_payment"),
+    path("cancel/", cancel_view, name="cancel_payment"),
 ]
