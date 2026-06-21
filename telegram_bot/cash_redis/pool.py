f28026dac6e4501785ebd9261fec280e787b2582
Roman Sokolov <gnonasis@gmail.com>
2026-06-21 12:36:20 +0300

    bugfix decode_responses=True

--- telegram_bot/cash_redis/pool.py
+++ telegram_bot/cash_redis/pool.py
@@ -6,8 +6,8 @@ redis_client: aioredis.Redis | None = None
 
 async def init_redis_pool()-> aioredis.Redis:
     global redis_pool, redis_client
-    redis_pool = aioredis.ConnectionPool.from_url(REDIS_URL)
-    return  aioredis.Redis(connection_pool=redis_pool, decode_responses=True)
+    redis_pool = aioredis.ConnectionPool.from_url(REDIS_URL, decode_responses=True)
+    return  aioredis.Redis(connection_pool=redis_pool)
 
 async def close_redis_pool():
     global redis_pool
